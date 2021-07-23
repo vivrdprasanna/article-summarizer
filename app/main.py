649d402ff64c14fd6d12ffe9283e0e640a1061fa
vivrdprasanna <59852610+vivrdprasanna@users.noreply.github.com>
2021-07-23 12:42:51 -0700

    ready for production deployment

---
+++ app/main.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, render_template
+import sklearn
+import pickle
+import pandas as pd
+import numpy as np
+import os
+from flask_cors import cross_origin
+from utils import get_base_url, allowed_file, and_syntax
+from spam_or_ham import predict
+from classifier import predict_with_percent
+
+# setup the webservver
+# port = 1234
+# base_url = get_base_url(port)
+# app = Flask(__name__, static_url_path=base_url+'static')
+
+app = Flask(__name__)
+
+IMAGE_FOLDER=os.path.join('static','images')
+app.config['UPLOAD_FOLDER']=IMAGE_FOLDER
+    
+
+
+# @app.route(base_url)
+def home():
+    return render_template('home.html')
+
+
+@app.route(base_url + "#", methods=["GET","POST"])
+@cross_origin()
+def result():
+    if request.method=="POST":
+        review = (request.form["Review"])
+        prediction = predict(review)
+#         _, certainty = predict_with_percent(review)
+#         certainty = round(certainty, 3) 
+        output=""
+        if not "not" in prediction:
+            output="spam!" # +  " certainty: " + str(certainty)
+        else:
+            output="not spam!" #+ " certainty: " + str(100 - certainty)
+    return render_template('home.html',prediction_text=f' {output}')
+    return render_template("home.html")
+
+
+
+
+
+if __name__ == "__main__":
+    # change the code.ai-camp.org to the site where you are editing this file.
+    print("Try to open\n\n    https://cocalc3.ai-camp.org" + base_url + '\n\n')
+    # remove debug=True when deploying it
+    app.run(host = '0.0.0.0', port=port, debug=True)
+    import sys; sys.exit(0)
+
+
---
+++ app/spam_or_ham.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+import wordcloud
+import pickle
+
+# helps in text preprocessing
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+# helps in model building
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.callbacks import EarlyStopping
+
+
+model = tf.keras.models.load_model("spam_model")
+with open('spam_model/tokenizer.pkl', 'rb') as input:
+    tokenizer = pickle.load(input)
+    
+def get_spam_or_ham(pred):
+    if pred == 1:
+        return 'spam!'
+    return 'not spam!'
+
+def predict(sms_original):
+    sms = np.array([sms_original])
+    sms_proc = tokenizer.texts_to_sequences(sms)
+    sms_proc = pad_sequences(sms_proc, maxlen = 8, padding='post')
+    pred = (model.predict(sms_proc) > 0.5).astype("int32").item()
+    return get_spam_or_ham(pred)
+
+
+    
+    
+
+    
